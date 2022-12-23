e1eac86358b9153f8c66af111dcf593ab64cd339
Bezrukov Kirill <doomseifer@mail.ru>
2022-12-23 19:40:34 +0400

    Elevator ready but stuttery

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Elevator : MonoBehaviour
+{
+    [SerializeField] GameObject platformToLift;
+    [SerializeField] GameObject refHeight;
+    Vector3 initPosition;
+    bool isMoving = false;
+    bool isLifted = false;
+    float delayTime = 3f;
+    float currentTime = 3f;
+    private void Start()
+    {
+        initPosition = platformToLift.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isMoving && currentTime > delayTime && !isLifted)
+            GoUp();
+        else
+        if (!isMoving && currentTime > delayTime)
+            GoDown();
+    }
+
+    private void GoUp()
+    {
+        isMoving = true;
+        platformToLift.transform.DOMove(refHeight.transform.position, 5f).onComplete = () =>
+        {
+            isLifted = true;
+            isMoving = false;
+            currentTime = 0f;
+        };
+    }
+
+    private void GoDown()
+    {
+        isMoving = true;
+        platformToLift.transform.DOMove(initPosition, 5f).onComplete = () =>
+        {
+            isLifted = false;
+            isMoving = false;
+            currentTime = 0f;
+        };
+    }
+
+    private void Update()
+    {
+        currentTime += Time.deltaTime;
+    }
+}
