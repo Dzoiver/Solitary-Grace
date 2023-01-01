b3ce2d28e8d15b2d1d3610bd61e9c1c0e2f618c9
Bezrukov Kirill <doomseifer@mail.ru>
2023-01-01 17:47:49 +0400

    3rd zone is started

---
+++ Assets/Scripts/ChangeLight.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLight : MonoBehaviour
+{
+    [SerializeField] Material skyboxDefault;
+    Color ambientColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ambientColor = new Color(0.36f, 0.34f, 0.36f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        RenderSettings.ambientLight = Color.white;
+        RenderSettings.skybox = skyboxDefault;
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
