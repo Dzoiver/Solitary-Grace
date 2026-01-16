c12afef055f264cb0aad373fe768b1fa5ac1c334
Kirill Bezrukov <tofukafujimoto@gmail.com>
2026-01-16 06:58:44 +0400

    Tape texture and effect added

--- Assets/Scripts/Tape.cs
+++ Assets/Scripts/Tape.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class Tape : MonoBehaviour
 {
+    [SerializeField] ParticleSystem particle;
+    [SerializeField] GameObject[] tapes;
+    BoxCollider boxCollider;
+    MeshCollider meshCollider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider>();
+        meshCollider = GetComponent<MeshCollider>();
     }
 
     public void RemoveTape()
     {
-        gameObject.SetActive(false);
+        particle.Play();
+        boxCollider.enabled = false;
+        meshCollider.enabled = false;
+        foreach (GameObject tape in tapes)
+        {
+            tape.SetActive(false);
+        }
     }
 
     // Update is called once per frame
